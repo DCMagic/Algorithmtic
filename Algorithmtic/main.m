be105880a107495253f1697cea8825063992de0b
DCMagic <1247757314@qq.com>
2021-07-05 22:33:21 +0800

    first commit

---
+++ Algorithmtic/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Algorithmtic
+//
+//  Created by a on 2021/7/5.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
